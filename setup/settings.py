2d498c251ca7a80557598c3e94f914cc712f9e1d
Davi Souza <davi.souza@precojusto.app.br>
2025-04-23 15:11:26 -0400

    [FEAT]: create new app django

--- setup/settings.py
+++ setup/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import os
-from pathlib import Path
+from pathlib import Path, os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "gallery",
 ]
 
 MIDDLEWARE = [
