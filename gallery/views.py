d77fda6e896d2124525de22510ae75d5c69f42ce
Davi Souza <davi.souza@precojusto.app.br>
2025-04-23 15:46:36 -0400

    [FEAT]: add first view with html on django

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Hello, Django!</h1><p>Bem vindo ao mundo Django</p>")
+    return render(request, "index.html")
