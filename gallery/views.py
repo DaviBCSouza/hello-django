9ef51560180d4b69f221bb6b6ef569ec06be5a98
Davi Souza <davi.souza@precojusto.app.br>
2025-04-23 17:40:14 -0400

    [FEAT]: add navigation on pages

--- gallery/views.py
+++ gallery/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "gallery/index.html")
+
+
+def imagem(request):
+    return render(request, "gallery/imagem.html")
